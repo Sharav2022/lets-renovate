c037dd5d92a35a8f762e3c17ea1785699e95e0e4
Sharav2022 <144260690+Sharav2022@users.noreply.github.com>
2024-03-31 02:31:46 +0530

    Add files via upload

---
+++ pages/work/index.js
@@ -0,0 +1,44 @@
+import { Services } from "../../src/globals/mock";
+import classes from "./work.module.css";
+import WorkCard from "../../src/components/core/WorkCard";
+import Animate from "../../src/components/core/Animate";
+import MetaInfo from "../../src/components/main/MetaInfo";
+
+const WorkPage = () => {
+  return (
+    <>
+      {MetaInfo()}
+      <section className={"container " + classes.service_section}>
+        <h2 className={classes.service_headline}>Our Work</h2>
+        <h4 className={classes.service_sub_headline}>
+          You’re invited to browse a selection of recently completed projects
+          below.
+        </h4>
+        <Animate>
+          <div className={classes.services}>
+            {Services.map((service) => (
+              <WorkCard {...service} />
+            ))}
+          </div>
+        </Animate>
+        <Animate>
+          <div className={classes.free_estimate}>
+            <div className={classes.txt}>
+              Create your dream home.
+              <br />
+              Tell us about your project today.
+            </div>
+            <div
+              className={classes.hb_btn}
+              onClick={() => router.push("about-us")}
+            >
+              Get a Free Estimate
+            </div>
+          </div>
+        </Animate>
+      </section>
+    </>
+  );
+};
+
+export default WorkPage;
